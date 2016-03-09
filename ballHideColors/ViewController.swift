b133ce6ba53c9ccefc0d4aad83cdd5b13deb3b14
Mathis <mathis@pc9.home>
2016-03-09 16:02:55 +0100

    Initial Commit

---
+++ ballHideColors/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ballHideColors
+//
+//  Created by Mathis on 09/03/16.
+//  Copyright © 2016 afm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
