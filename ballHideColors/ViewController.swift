bfeba991317603cee3beee58192ecb086ee16489
Mathis <mathis@pc9.home>
2016-03-09 19:07:35 +0100

    Added files for ballHide

--- ballHideColors/ViewController.swift
+++ ballHideColors/ViewController.swift
@@ -9,6 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var hideRedButton: UIButton!
+    @IBOutlet weak var hideBlueButton: UIButton!
+    @IBOutlet weak var redBall: UIImageView!
+    @IBOutlet weak var blueBall: UIImageView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +27,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func clickHideRed(sender: AnyObject) {
+    
+    hideRedButton.hidden = false
+    hideBlueButton.hidden = false
+    redBall.hidden = true
+    blueBall.hidden = false
+    
+    }
+    
+    @IBAction func clickHideBlue(sender: AnyObject) {
+    
+    hideBlueButton.hidden = false
+    hideRedButton.hidden = false
+    blueBall.hidden = true
+    redBall.hidden = false
+        
+    }
 
+    
 }
 
